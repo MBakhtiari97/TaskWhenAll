fcedeeeba264cb5ecd3c9a15ef65de89c67129ea
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2024-05-22 18:13:53 +0330

    Add project files.

---
+++ TWA/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await FirstOperation();
+        await SecondOperation();
+        await ThirdOperation();
+
+        var FirstTask = FirstOperation();
+        var SecondTask = SecondOperation();
+        var ThirdTask = ThirdOperation();
+            
+        await Task.WhenAll(FirstTask, SecondTask, ThirdTask);
+        //another task
+    }
+    public static async Task FirstOperation()
+    {
+        Console.WriteLine("First Operation Done!");
+    }
+
+    public static async Task SecondOperation()
+    {
+        Console.WriteLine("Second Operation Done!");
+    }
+
+    public static async Task ThirdOperation()
+    {
+        Console.WriteLine("Third Operation Done!");
+    }
+}
